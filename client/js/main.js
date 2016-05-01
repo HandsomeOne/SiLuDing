a0cf077f28ac5dc9057d465ac3e0ea798b7c244d
HandsomeOne <i@handsomeone.com>
2016-05-01 23:40:13 +0800

    remastered

--- client/js/main.js
+++ client/js/main.js
@@ -1,36 +1,28 @@
 'use strict';
 
-try {
-  screen.lockOrientation('landscape');
-  screen.msLockOrientation('landscape');
-} catch (e) {
-  console.log(e);
-}
-
-window.gameTypes = {
+var gameTypes = {
   SiLuDing: '四路顶',
   LiuLuDing: '六路顶',
 };
 
-for (var type in window.gameTypes) {
+for (var type in gameTypes) {
   var a = document.createElement('a');
-  a.innerHTML = window.gameTypes[type];
+  a.innerHTML = gameTypes[type];
   a.href = '?gameType=' + type;
   $('#type-select').appendChild(a);
 }
 
-window.query = parseQuery(location.search);
-window.gameType = window.query.gameType;
-if (!(window.gameType in window.gameTypes)) {
+var query = parseQuery(location.search);
+var gameType = query.gameType;
+if (!(gameType in gameTypes)) {
   $('#type-select').style.display = 'block';
   document.title = '请选择游戏类型...';
 } else {
-  window.room = window.query.room;
-  if (window.room) {
+  if (query.room) {
     $('#create-private-room').style.display = 'none';
   }
 
-  document.title = window.gameTypes[window.gameType];
+  document.title = gameTypes[gameType];
   initBackgroundColor();
   if (localStorage.name && localStorage.color) {
     $('#settings').style.display = 'none';
@@ -43,10 +35,10 @@ if (!(window.gameType in window.gameTypes)) {
     localStorage.name = $('#nickname').value;
     localStorage.color = $('#color').value;
     $('#settings').style.display = 'none';
-    if (window.socket) {
+    if (socket) {
       initStyle();
-      window.socket.disconnect();
-      window.socket.connect();
+      socket.disconnect();
+      socket.connect();
     } else {
       init();
     }
@@ -67,19 +59,17 @@ function initStyle() {
   $('#cancel-settings').style.display = 'block';
 }
 function initSocket() {
-  window.socket = window.io(location.origin + '/' + window.gameType);
-  var socket = window.socket;
+  window.socket = io(location.origin + '/' + gameType);
   socket.on('connect', function () {
-    var player = {
+    socket.emit('join', {
       id: socket.id,
       name: localStorage.name,
       color: localStorage.color,
-      room: window.room,
-    };
-    socket.emit('join', player);
+      room: query.room,
+    });
   });
   socket.on('start', function (data) {
-    window.game = new window[window.gameType + 'Client'](socket, data, $('canvas'));
+    window.game = new window[gameType + 'Client'](socket, data, $('canvas'));
     $('#out-game-controls').style.display = 'none';
     $('#in-game-controls').style.display = 'block';
     handleProgress(data.active);
@@ -97,20 +87,20 @@ function initSocket() {
   socket.on('update', function (data) {
     if ('active' in data) {
       handleProgress(data.active);
-      if (data.active === window.game.index) {
-        var oldTitle = window.gameTypes[window.gameType];
+      if (data.active === game.index) {
+        var oldTitle = gameTypes[gameType];
         if (!document.hasFocus()) {
-          window.blink = setInterval(function () {
+          blink = setInterval(function () {
             document.title = (document.title === oldTitle) ? '轮到你了！' : oldTitle;
             if (document.hasFocus()) {
               document.title = oldTitle;
-              clearInterval(window.blink);
+              clearInterval(blink);
             }
           }, 1000);
         }
       } else {
-        document.title = window.gameTypes[window.gameType];
-        clearInterval(window.blink);
+        document.title = gameTypes[gameType];
+        clearInterval(blink);
       }
     }
   });
@@ -127,29 +117,29 @@ function initListeners() {
     e.preventDefault();
     var content = $('#content');
     if (content.value.trim()) {
-      window.socket.emit('chat', content.value.trim());
+      socket.emit('chat', content.value.trim());
     }
     content.value = '';
   });
   $('#ready').addEventListener('click', function () {
-    window.socket.emit('ready');
+    socket.emit('ready');
     $('#ready').style.display = 'none';
     $('#unready').style.display = 'block';
   });
   $('#unready').addEventListener('click', function () {
-    window.socket.emit('unready');
+    socket.emit('unready');
     $('#unready').style.display = 'none';
     $('#ready').style.display = 'block';
   });
   $('#skip').addEventListener('click', function () {
-    if (window.game.active === window.game.index) {
-      window.socket.emit('skip');
+    if (game.active === game.index) {
+      socket.emit('skip');
       $('#skip').style.backgroundColor = 'rgba(0,0,0,0.25)';
-      clearInterval(window.interval);
+      clearInterval(interval);
     }
   });
   $('#surrender').addEventListener('click', function () {
-    window.socket.emit('surrender');
+    socket.emit('surrender');
   });
 
   $('#change-settings').addEventListener('click', function () {
@@ -157,15 +147,15 @@ function initListeners() {
   });
   $('#change-type').addEventListener('click', function () {
     if (confirm('更改游戏类型将退出当前房间！')) {
-      window.socket.disconnect();
-      window.onbeforeunload = null;
+      socket.disconnect();
+      onbeforeunload = null;
       $('#type-select').style.display = 'block';
     }
   });
   $('#create-private-room').addEventListener('click', function () {
     if (confirm('创建私人房间将退出当前房间！')) {
-      window.onbeforeunload = null;
-      location.href = location.search + '&room=' + window.socket.id;
+      onbeforeunload = null;
+      location.href = location.search + '&room=' + socket.id;
     }
   });
   $('#cancel-settings').addEventListener('click', function () {
@@ -175,8 +165,8 @@ function initListeners() {
     localStorage.backgroundColor = this.value;
     initBackgroundColor();
   });
-  window.addEventListener('resize', resize);
-  window.onbeforeunload = function () {
+  addEventListener('resize', resize);
+  onbeforeunload = function () {
     return '确认离开？';
   };
 }
@@ -189,16 +179,16 @@ function initBackgroundColor() {
   $('#controls').style.color = localStorage.backgroundColor;
 }
 function handleProgress(active) {
-  clearInterval(window.interval);
-  $('#skip').style.backgroundColor = (active === window.game.index) ? localStorage.color : 'rgba(0,0,0,0.25)';
+  clearInterval(interval);
+  $('#skip').style.backgroundColor = (active === game.index) ? localStorage.color : 'rgba(0,0,0,0.25)';
   $('#progress-inner').style.width = '100%';
-  $('#progress-inner').style.backgroundColor = window.game.players[active].color;
+  $('#progress-inner').style.backgroundColor = game.players[active].color;
   var start = new Date().getTime();
-  window.interval = setInterval(function () {
+  interval = setInterval(function () {
     var time = new Date().getTime();
     $('#progress-inner').style.width = 100 - (time - start) / 300 + '%';
     if (time >= start + 30000) {
-      clearInterval(window.interval);
+      clearInterval(interval);
     }
   }, 1000 / 60);
 }
